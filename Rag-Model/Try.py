fcf18851c7d8cb7c2d51cb5452777779656ed62f
Aakarsh Lohani <lohaniaakarsh2017@gmail.com>
2025-06-26 23:48:11 +0530

    Creating chunks completed

--- Rag-Model/Try.py
+++ Rag-Model/Try.py
@@ -1,7 +1,12 @@
 import getpass
 import os
 import bs4
-from langchain import hub # hub
+from langchain import hub # centralized repository
+from langchain_community.document_loaders import WebBaseLoader #use to extract and structure and content from web pages
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
 
 ##################################################################
 #environment variables to start logging traces:
@@ -38,5 +43,19 @@ vector_store = InMemoryVectorStore(embeddings)
 
 
 #######################################################################
+#Load and chunk contents of the blog
 
+loader = WebBaseLoader(
+  web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+  bs_kwargs=dict(
+    parse_only= bs4.SoupStrainer(
+      class_ = ("post-contnet", "post-title", "post-header")
+    )
+  ),
+)
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+all_splits = text_splitter.split_documents(docs)
 
